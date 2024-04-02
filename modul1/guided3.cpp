420f41ffb8c20e93f527c037a7b1e10f8608fec1
Muhamad Nasrulloh <163017201+nasruel23@users.noreply.github.com>
2024-04-02 22:23:05 +0700

    Add files via upload

---
+++ modul1/guided3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //deklarasi dan inisialisasi array
+    int nilai[5] = {23, 50, 34, 78, 90};
+
+    //mencetak isi array
+    cout << "Isi array pertama: " << nilai [0]<< endl;
+    cout << "Isi array kedua: " << nilai [1]<< endl;
+    cout << "Isi array ketiga: " << nilai [2]<< endl;
+    cout << "Isi array keempat: " << nilai [3]<< endl;
+    cout << "Isi array kelima: " << nilai [4]<< endl;
+    return 0;
+}
